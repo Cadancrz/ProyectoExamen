f962475510b4dcbef82e2ede9995cadd3c9e5879
Cadancrz <cadan.crz@gmail.com>
2023-07-12 12:13:03 -0600

    index

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Configuración de rutas y middleware
+
+// Puerto de escucha
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor Express.js en funcionamiento en http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('PROYECTO upnfm');
+});
