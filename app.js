8fa587551c739eea7ec7f431df104666732e0f21
Cadancrz <cadan.crz@gmail.com>
2023-07-14 02:22:01 -0600

    Login y Formularios principales listos

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -20,7 +23,7 @@ db.connect((err) => {
       console.log('Conexión exitosa a la base de datos');
 });
 
-
+// ----------------------------------------------------------------
 app.post('/adduser', (req, res) => {
     let data = req.body;
     console.log(data);
@@ -35,10 +38,62 @@ app.post('/adduser', (req, res) => {
    }); 
 });
 
+//----------------------------------------------------------------
+
+// login route handler
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+  
+    // Consultar la base de datos para verificar si el nombre de usuario y contraseña coinciden con un registro de usuario
+    db.query('SELECT rol FROM usuarios WHERE email = ? AND password = ?', [email, password], (error, results) => {
+      if (error) {
+        console.error(error);
+        return res.redirect('/error'); // Manejar el error de la consulta a la base de datos
+      }
+  
+      if (results.length === 0) {
+        console.log('Usuario no encontrado');
+        return res.redirect('/error'); // Manejar el caso de usuario no encontrado
+      }
+  
+      const user = results[0];
+    
+      // Check the role and redirect to the corresponding menu page
+      if (user.rol === 'docente') {
+        res.redirect('/menudocente');
+      } else if (user.rol === 'estudiante') {
+        res.redirect('/menuestudiante');
+      } else {
+        console.log(user.rol);
+      }
+    });
+  });
+  
+  
+  app.get('/menudocente', (req, res) => {
+    // Render the menudocente.html file
+    res.sendFile(__dirname + '/views/menudocente.html');
+  });
+  
+  app.get('/menuestudiante', (req, res) => {
+    // Render the menuestudiante.html file
+    res.sendFile(__dirname + '/views/menuestudiante.html');
+  });
+  
+//----------------------------------------------------------------
+
+app.use(express.static(path.join(__dirname, 'static')));
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+  });
+  
+  app.get('/addUserForm', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/addUserForm.html'));
+  });
 
 app.use(express.static('statics'))
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/login.html');
 });
 
 
