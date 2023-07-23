78a0871c07e6f6475fad95d3e9f636a3b489a86c
Cadancrz <cadan.crz@gmail.com>
2023-07-22 22:17:32 -0600

    Preguntas listas, pendiente validar navegacion

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const userController = require("./controllers/userController");
 const loginController = require("./controllers/loginController");
 const temasController = require("./controllers/temasController"); 
+const examenController = require("./controllers/examenController");
 
 
 const mysql = require('mysql');
@@ -24,7 +25,7 @@ app.use(session({
 }));
 
 app.post('/login', loginController.HomePostLogin);
-app.post('/mdocente', auth,loginController.HomePostLogin);
+app.get('/mdocente', auth,loginController.HomePostLogin);
 
 
 app.post('/adduser',userController.adduser);  
@@ -32,13 +33,33 @@ app.get('/listarUSR',userController.listarUSR);
 
 app.get('/listarTemas',temasController.listarTemas);
 app.post('/inserttema', temasController.inserttema);
-  
-  app.get('/menudocente', (req, res) => {
-    const user = req.session.user;
-    res.render('menudocente',{ user }); // Renderizar la vista menudocente pasando los valores de nombre y rol
-  });
+
+
+app.get('/tablaExamenes', examenController.tablaExamenes);
+app.post('/guardarExamen', examenController.guardarExamen);
+app.get('/formularioCrearPreguntas', examenController.formularioCrearPreguntas);
+app.get('/bancoPreguntas', examenController.bancoPreguntas);
+app.post('/guardarPregunta', examenController.guardarPregunta);
+
+
+
+app.get('/menudocente', (req, res) => {
+  const user = req.session.user;
+
+  // Verificar si el objeto 'user' está presente en la session
+  if (!user || !user.nombre) {
+    // Si el objeto 'user' no está presente o no tiene la propiedad 'nombre', redirige al login o muestra un mensaje de error.
+    return res.redirect('/login'); // O puedes mostrar una página de error personalizada
+  }
+
+  res.render('menudocente', { user }); // Renderizar la vista menudocente pasando los valores de nombre y rol
+});
   app.get('/menuestudiante', (req, res) => {
     const user = req.session.user;
+    if (!user || !user.nombre) {
+      // Si el objeto 'user' no está presente o no tiene la propiedad 'nombre', redirige al login o muestra un mensaje de error.
+      return res.redirect('/login'); // O puedes mostrar una página de error personalizada
+    }
     res.render('menuestudiante',{ user });  // Enviar el archivo menuestudiante.ejs
   });
 
@@ -55,11 +76,47 @@ app.get('/login', (req, res) => {
   
   app.get('/tablaestudiantes', (req, res) => {
     const user = req.session.user;
+    if (!user || !user.nombre) {
+      // Si el objeto 'user' no está presente o no tiene la propiedad 'nombre', redirige al login o muestra un mensaje de error.
+      return res.redirect('/login'); // O puedes mostrar una página de error personalizada
+    }
     res.render('tablaestudiantes',{ user }); // Renderizar la vista menudocente pasando los valores de nombre y rol
   });
   app.get('/tablatemas', (req, res) => {
     const user = req.session.user;
+    if (!user || !user.nombre) {
+      // Si el objeto 'user' no está presente o no tiene la propiedad 'nombre', redirige al login o muestra un mensaje de error.
+      return res.redirect('/login'); // O puedes mostrar una página de error personalizada
+    }
     res.render('tabladetemas',{ user }); // Renderizar la vista menudocente pasando los valores de nombre y rol
+  });
+
+  app.get('/formularioCrearExamenes', (req, res) => {
+    const user = req.session.user;
+    if (!user || !user.nombre) {
+      // Si el objeto 'user' no está presente o no tiene la propiedad 'nombre', redirige al login o muestra un mensaje de error.
+      return res.redirect('/login'); // O puedes mostrar una página de error personalizada
+    }
+    res.render('formularioCrearExamenes',{ user }); // Renderizar la vista menudocente pasando los valores de nombre y rol
+  });
+
+  
+  app.get('/formularioCrearPreguntas', (req, res) => {
+    const user = req.session.user;
+    if (!user || !user.nombre) {
+      // Si el objeto 'user' no está presente o no tiene la propiedad 'nombre', redirige al login o muestra un mensaje de error.
+      return res.redirect('/login'); // O puedes mostrar una página de error personalizada
+    }
+    res.render('formularioCrearPreguntas',{ user }); // Renderizar la vista menudocente pasando los valores de nombre y rol
+  });
+
+  app.get('/formularioBancoPreguntas', (req, res) => {
+    const testId = req.query.testId;
+    if (!user || !user.nombre) {
+      // Si el objeto 'user' no está presente o no tiene la propiedad 'nombre', redirige al login o muestra un mensaje de error.
+      return res.redirect('/login'); // O puedes mostrar una página de error personalizada
+    }
+    res.render('formularioBancoPreguntas',{ testId }); // Renderizar la vista menudocente pasando los valores de nombre y rol
   });
 
   // ---------------------------------------------------------------- GET DE INICIO ----------------------------------------------------------------
